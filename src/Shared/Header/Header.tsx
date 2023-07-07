c4f2d5cbad1751bd1cb239ada01508be1c58a9cb
Abdullah Al Masud <masudctg00@gmail.com>
2023-07-07 17:20:02 +0600

    done

--- src/Shared/Header/Header.tsx
+++ src/Shared/Header/Header.tsx
@@ -5,7 +5,6 @@ const Header = () => {
     return (
         <div className='flex justify-center items-center  py-5 bg-cyan-950 text-white font-'>
             <h1 className='text-3xl font-bold'>Information App</h1>
-           
         </div>
     );
 };
